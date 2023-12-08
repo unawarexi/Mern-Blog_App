2a2f7dddb27f6edf68ff6f1f347c57851d6891a4
unawarexi <unawarexi@gmail.com>
2023-12-08 05:03:06 +0100

    route

--- server/routes/UserRoutes.js
+++ server/routes/UserRoutes.js
@@ -1,8 +1,7 @@
-const express = require("express")
+const express = require("express");
+const router = express.Router();
+const {registerUser} = require("../controllers/UserControllers.js")
 
-const router =  express.Router();
-const enrollUser = require("../controllers/UserControllers") 
-
-router.post('/register',  enrollUser);
+router.post("/register",  registerUser);
 
 module.exports = router;
