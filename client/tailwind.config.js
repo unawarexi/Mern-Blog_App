1e77ba927108241d87c9ddea9c96dd77d4fcff72
unawarexi <unawarexi@gmail.com>
2024-12-27 10:59:33 +0100

    not completed | integrating cloudinary

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -31,6 +31,14 @@ export default {
       }
     },
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/typography"), require("daisyui")],
+
+  daisyui: {
+    themes: [], // true: all themes | false: only light + dark | array: specific themes like this ["light", "dark", "cupcake"]
+    base: false, // applies background color and foreground color for root element by default
+    styled: true, // include daisyUI colors and design decisions for all components
+    utils: true, // adds responsive and modifier utility classes
+    prefix: "d-",
+  },
 }
 
